a7d2691d73c611a7d661753d9eeea1c19a7e4335
LancelotLiu <lancelot.yl.liu@gmail.com>
2018-01-23 10:32:31 +0800

    fixing error in eclipse

--- cap-db/src/main/java/com/iisigroup/cap/jdbc/CapNamedJdbcTemplate.java
+++ cap-db/src/main/java/com/iisigroup/cap/jdbc/CapNamedJdbcTemplate.java
@@ -232,6 +232,34 @@ public class CapNamedJdbcTemplate extends NamedParameterJdbcTemplate {
     }
 
     /**
+     * 查詢筆數專用
+     * 
+     * @param sqlId
+     *            sqlId
+     * @param args
+     *            args
+     * @return int
+     * @throws GWException
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public int queryForInt(String sqlId, Map<String, ?> args) {
+        StringBuffer sql = new StringBuffer((String) sqlp.getValue(sqlId, sqlId));
+        sql.append(' ').append(sqltemp.getValue(CapJdbcConstants.SQL_QUERY_SUFFIX, ""));
+        if (logger.isTraceEnabled()) {
+            logger.trace(new StringBuffer("SqlId=").append(sqlp.containsKey(sqlId) ? sqlId : "").append("\n\t").append(CapDbUtil.convertToSQLCommand(sql.toString(), args)).toString());
+        }
+        long cur = System.currentTimeMillis();
+        try {
+            Integer result = super.queryForObject(sql.toString(), (Map) args, Integer.class);
+            return result == null ? 0 : result.intValue();
+        } catch (Exception e) {
+            throw new CapDBException(e, causeClass);
+        } finally {
+            logger.info("CapNamedJdbcTemplate spend {} ms", (System.currentTimeMillis() - cur));
+        }
+    }
+
+    /**
      * 查詢，查詢結果為Map<key,value>
      * 
      * @param sqlId
