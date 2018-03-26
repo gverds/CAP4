5370d7a91142f0c2b8b208ddf19572828d7f8d61
LancelotLiu <lancelot.yl.liu@gmail.com>
2018-03-26 14:09:15 +0800

    #4819

--- cap-base/src/main/java/com/iisigroup/cap/base/dao/impl/SequenceDaoImpl.java
+++ cap-base/src/main/java/com/iisigroup/cap/base/dao/impl/SequenceDaoImpl.java
@@ -48,7 +48,7 @@ public class SequenceDaoImpl extends GenericDaoImpl<Sequence> implements Sequenc
     public Sequence findBySeqNode(String seqNode) {
         Map<String, Object> args = new HashMap<String, Object>();
         args.put("seqNode", seqNode);
-        return getNamedJdbcTemplate().queryForObject("sequence_findBySeqNode", args, new RowMapper<Sequence>() {
+        return getNamedJdbcTemplate().queryForObject("Sequence.findBySeqNode", "", args, new RowMapper<Sequence>() {
             @Override
             public Sequence mapRow(ResultSet rs, int rowNum) throws SQLException {
                 Sequence seq = new Sequence();
