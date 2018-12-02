3ba4d5b4402568c0329afa09aeaad7c62b469b03
LancelotLiu <lancelot.yl.liu@gmail.com>
2018-12-02 11:41:04 +0800

    remove useless dependencies, class & revert CapSqlSearchQueryProvider

--- cap-db/src/main/java/com/iisigroup/cap/jdbc/support/CapSqlSearchQueryProvider.java
+++ cap-db/src/main/java/com/iisigroup/cap/jdbc/support/CapSqlSearchQueryProvider.java
@@ -180,7 +180,7 @@ public class CapSqlSearchQueryProvider {
             sb.append(key).append(" IS NOT NULL ");
             break;
         case IN:
-            sb.append(key).append(" IN (:").append(paramKey).append(')');
+            sb.append(key).append(" IN :").append(paramKey);
             params.put(paramKey, asCollection(value));
             break;
         case LIKE:
